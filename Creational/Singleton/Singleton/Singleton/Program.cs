1d9804f136593c2284f3e4e8de752c26d19d433f
Muhammad Ali <ali.khan@CooperativeComputing.com>
2024-11-10 12:35:07 +0500

    Added Singletone

---
+++ Creational/Singleton/Singleton/Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var obj = Singleton.GetInstance();
+var obj1 = Singleton.GetInstance();
+var obj2 = Singleton.GetInstance();
+
+Console.WriteLine(Singleton.Count);
+
+public class Singleton
+{
+    private static Singleton _instance;
+    public static int Count; 
+
+    private Singleton()
+    {
+        Count++;
+    }
+
+    public static Singleton GetInstance()
+    {
+        if(_instance is null) _instance = new Singleton();
+        return _instance;
+    }
+
+}
